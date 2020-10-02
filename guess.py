a7cf5e8c35f332b6cfdf242b5a6fbccae6e69b86
Mahmoud Amr <mahmoudamr@localhost.localdomain>
2020-10-02 15:20:35 +0200

    Made a main function with interpreter to force use Python3

--- guess.py
+++ guess.py
@@ -1,24 +1,27 @@
+#!usr/bin/env python3
+
 import random
 import colorama
 
-randomNum = random.randint(1, 20)
-playerNumber = int(input(colorama.Fore.GREEN + 'Please enter your guess: '))
 
-
-def compareNums(playerNumber):
+def compareNums(playerNumber, randomNum):
     if (playerNumber == randomNum):
-        print(colorama.Fore.GREEN + "Good Job! You have guessed it correctly!")
+        print(colorama.Fore.GREEN + "Good Job! You have guessed it correctly! The number was:" + str(randomNum))
         print(colorama.Fore.BLUE + "You won!")
     elif (playerNumber > randomNum):
         print(colorama.Fore.YELLOW + "Too high!")
         playerNumber = int(
             input(colorama.Fore.GREEN + 'Please enter the number again: '))
-        compareNums(playerNumber)
+        compareNums(playerNumber, randomNum)
     else:
         print(colorama.Fore.RED + "Too low!")
         playerNumber = int(
             input(colorama.Fore.GREEN + 'Please enter the number again: '))
-        compareNums(playerNumber)
+        compareNums(playerNumber, randomNum)
 
+def main():
+    randomNum = random.randint(1, 20)
+    playerNumber = int(input(colorama.Fore.GREEN + 'Please enter your guess: '))
+    compareNums(playerNumber, randomNum)
 
-compareNums(playerNumber)
+if __name__ == "__main__": main()
