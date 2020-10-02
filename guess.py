03b27cda2aeadd3975b7588cf17ce349fd3b3923
Mohit Sharma <shash199655@gmail.com>
2020-10-02 18:28:13 +0530

    added colors

--- guess.py
+++ guess.py
@@ -1,19 +1,24 @@
 import random
+import colorama
 
-randomNum = random.randint(1,20)
-playerNumber = int(input('Please enter your guess: '))
+randomNum = random.randint(1, 20)
+playerNumber = int(input(colorama.Fore.GREEN + 'Please enter your guess: '))
+
 
 def compareNums(playerNumber):
     if (playerNumber == randomNum):
-        print("Good Job! You have guessed it correctly!")
-        print("You won!")
+        print(colorama.Fore.GREEN + "Good Job! You have guessed it correctly!")
+        print(colorama.Fore.BLUE + "You won!")
     elif (playerNumber > randomNum):
-        print("Too high!")
-        playerNumber = int(input('Please enter the number again: '))
+        print(colorama.Fore.YELLOW + "Too high!")
+        playerNumber = int(
+            input(colorama.Fore.GREEN + 'Please enter the number again: '))
         compareNums(playerNumber)
     else:
-        print("Too low!")
-        playerNumber = int(input('Please enter the number again: '))
+        print(colorama.Fore.RED + "Too low!")
+        playerNumber = int(
+            input(colorama.Fore.GREEN + 'Please enter the number again: '))
         compareNums(playerNumber)
+
 
 compareNums(playerNumber)
